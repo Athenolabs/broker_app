210589c6356660f8bd3fe745602d0113362bc252
bobzz-zone <bobzz.zone@gmail.com>
2016-02-17 21:17:20 +0700

    commit changes validate

--- broker_app/commision/doctype/data_komisi/data_komisi.py
+++ broker_app/commision/doctype/data_komisi/data_komisi.py
@@ -7,4 +7,29 @@ import frappe
 from frappe.model.document import Document
 
 class DataKomisi(Document):
-	pass
+	def validate(self):
+		listing_count=0
+		selling_count=0
+		koor_count=0
+		for marketing in self.commision_list:
+			if marketing.type == "Listing":
+				listing_count=listing_count+1
+			elif marketing.type == "Selling":
+				selling_count=selling_count+1
+			elif marketing.type == "Koordinator":
+				koor_count=koor_count+1
+		if listing_count>2 or selling_count>2 or koor_count>2:
+			frappe.throw("Maximum 2 marketing untuk Selling / Listing / Koordinator")
+
+	def on_submit(self):
+		pass
+
+	def get_marketing_list(self):
+		if self.data_transaksi:
+			data = frappe.db.sql("""select marketing,nama,kantor,type from `tabTransaction Marketing Detail` where parent="{}" """.format(self.data_transaksi),as_list=1)
+			for row in data:
+				dt = self.append('commision_list', {})
+				dt.marketing=row[0]
+				dt.nama=row[1]
+				dt.kantor=row[2]
+				dt.type=row[3]
--- broker_app/sales/doctype/data_transaksi/data_transaksi.py
+++ broker_app/sales/doctype/data_transaksi/data_transaksi.py
@@ -7,4 +7,13 @@ import frappe
 from frappe.model.document import Document
 
 class DataTransaksi(Document):
-	pass
+	def validate(self):
+		listing_count=0
+		selling_count=0
+		for marketing in self.marketing_list:
+			if marketing.type == "Listing":
+				listing_count=listing_count+1
+			elif marketing.type == "Selling":
+				selling_count=selling_count+1
+		if listing_count>2 or selling_count>2:
+			frappe.throw("Maximum 2 marketing untuk Selling / Listing")
