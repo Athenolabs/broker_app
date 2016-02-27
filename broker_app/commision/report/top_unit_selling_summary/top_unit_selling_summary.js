f7ce76550792c3ad75f2c448817e8db96365f9cb
bobzz-zone <bobzz.zone@gmail.com>
2016-02-27 15:55:11 +0700

    tut report v001

--- broker_app/commision/report/top_unit_selling_summary/top_unit_selling_summary.js
+++ broker_app/commision/report/top_unit_selling_summary/top_unit_selling_summary.js
@@ -3,6 +3,26 @@
 
 frappe.query_reports["Top Unit Selling Summary"] = {
 	"filters": [
-
+		{
+			"fieldname":"from",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.defaults.get_default("year_start_date"),
+			"width": "80",
+			"reqd":1
+		},
+		{
+			"fieldname":"to",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": get_today(),
+			"reqd":1
+		},
+		{
+			"fieldname":"branch",
+			"label": __("Branch"),
+			"fieldtype": "Link",
+			"options": "Branch"
+		}
 	]
 }
--- broker_app/commision/report/top_unit_selling_summary/top_unit_selling_summary.py
+++ broker_app/commision/report/top_unit_selling_summary/top_unit_selling_summary.py
@@ -6,4 +6,13 @@ import frappe
 
 def execute(filters=None):
 	columns, data = [], []
+	branch=""
+	if filters.get("branch"):
+		branch = """ and dk.branch= "{}" """.format(filters.get("branch"))
+	data=frappe.db.sql("""select cmd.marketing,m.nama,sum(point) 
+		from `tabTUT` cmd 
+		left join `tabData Komisi` dk on cmd.parent=dk.name 
+		left join `tabMarketing` m on cmd.marketing=m.name 
+		where cmd.docstatus=1 and cmd.posting_date between "{}" and "{}" {} """.format(filters.get("from"),filters.get("to"),branch))
+	columns=["No Marketing:Marketing/Link:200","Nama:Data:200","Total:Float:200"]
 	return columns, data
