7cbb28bd68d955c9d7b4f6991dbb920991615af0
bobzz-zone <bobzz.zone@gmail.com>
2016-03-01 11:45:12 +0700

    pv summary report fix

--- broker_app/commision/report/pv_summary/pv_summary.py
+++ broker_app/commision/report/pv_summary/pv_summary.py
@@ -14,6 +14,6 @@ def execute(filters=None):
 		from `tabPV` cmd 
 		left join `tabData Komisi` dk on cmd.parent=dk.name 
 		left join `tabMarketing` m on cmd.marketing=m.name 
-		where cmd.docstatus=1 and cmd.posting_date between "{}" and "{}" {} """.format(filters.get("from"),filters.get("to"),branch))
+		where cmd.docstatus=1 and cmd.posting_date between "{}" and "{}" {} group by cmd.marketing""".format(filters.get("from"),filters.get("to"),branch))
 	columns=["No Marketing:Marketing/Link:200","Nama:Data:200","Total:Currency:200"]
 	return columns, data
