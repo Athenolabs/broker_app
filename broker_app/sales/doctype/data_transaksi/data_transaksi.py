95338bfecf24bb852e962f3763e3e04524bd6241
bobzz-zone <bobzz.zone@gmail.com>
2016-04-15 13:02:43 +0700

    deadline update

--- broker_app/sales/doctype/data_transaksi/data_transaksi.py
+++ broker_app/sales/doctype/data_transaksi/data_transaksi.py
@@ -17,3 +17,54 @@ class DataTransaksi(Document):
 				selling_count=selling_count+1
 		if listing_count>1 or selling_count>2:
 			frappe.throw("Maximum 2 marketing untuk Selling dan 1 untuk Listing")
+
+	def on_cancel(self):
+		for row in self.deadline:
+			opts={
+				"owner": row.name,
+				"starts_on": row.tanggal,
+				"subject": ('Follow up Deadline ' + cstr(self.name)),
+				"description": row.note
+			}
+			self.delete_events()
+
+	def on_submit(self):
+		for row in self.deadline:
+			opts={
+				"owner": row.name,
+				"starts_on": row.tanggal,
+				"subject": ('Follow up Deadline ' + cstr(self.name)),
+				"description": row.note
+			}	
+			self._add_calendar_event(opts)
+
+	def delete_events(self):
+		events = frappe.db.sql_list("""select name from `tabEvent`
+			where ref_type=%s and ref_name=%s""", (self.doctype, self.name))
+		if events:
+			frappe.db.sql("delete from `tabEvent` where name in (%s)"
+				.format(", ".join(['%s']*len(events))), tuple(events))
+
+			frappe.db.sql("delete from `tabEvent Role` where parent in (%s)"
+				.format(", ".join(['%s']*len(events))), tuple(events))
+
+	def _add_calendar_event(self, opts):
+		opts = frappe._dict(opts)
+
+		if self.contact_date:
+			event = frappe.get_doc({
+				"doctype": "Event",
+				"owner": opts.owner or self.owner,
+				"subject": opts.subject,
+				"description": opts.description,
+				"starts_on":  self.contact_date,
+				"event_type": "Private",
+				"ref_type": self.doctype,
+				"ref_name": self.name
+			})
+
+			event.insert(ignore_permissions=True)
+
+			if frappe.db.exists("User", self.contact_by):
+				frappe.share.add("Event", event.name, self.contact_by,
+					flags={"ignore_share_permission": True})
